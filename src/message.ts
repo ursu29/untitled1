cbd86d411124be5da170c49e05bc601529c3a9f9
Alexey Avdeev <alexey.avdeev@sidenis.com>
2019-12-18 13:10:11 +0300

    removed icons

--- src/message.ts
+++ src/message.ts
@@ -2,7 +2,6 @@ import { message } from 'antd'
 
 const GRAPHQL_PREFIX = 'GraphQL error: Unexpected error value: "'
 const trimError = (data: any) => {
-  console.log(data)
   const text = data.toString()
   if (text.includes(GRAPHQL_PREFIX)) {
     return text.substring(text.indexOf(GRAPHQL_PREFIX) + GRAPHQL_PREFIX.length, text.length - 1)
