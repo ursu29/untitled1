8d38afd3c2bffc91b83e97a7b3e9a399a98c17af
Alexander Vygodchikov <Alexander.Vygodchikov@sidenis.com>
2020-04-24 14:28:16 +0300

    add new statistics page

--- src/components/Statistics/StatisticsPage.tsx
+++ src/components/Statistics/StatisticsPage.tsx
@@ -1,46 +1,79 @@
-import { useQuery } from '@apollo/react-hooks'
 import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
-import BarChart from '../UI/BarChart'
-import Tabs from '../UI/Tabs'
+import { Typography, Row, Col } from 'antd'
+
 import { Experience, Level, Skill } from '../../types'
 import Skeleton from '../UI/Skeleton'
 import PageContent from '../UI/PageContent'
+import BarChart from '../UI/BarChart'
+import { getSkillLink } from '../../paths'
 
+/**
+ * Queries definition
+ */
 const query = gql`
-  query getExperiences($input: ExperiencesInput) {
+  query getSkills($input: SkillsInput) {
     levels {
       id
       name
       index
     }
-    experiences(input: $input) {
+    skills(input: $input) {
       id
-      level {
+      name
+      experiences {
         id
-        index
-      }
-      skill {
-        id
-        name
+        level {
+          id
+        }
       }
     }
   }
 `
 
+/**
+ * Types definition
+ */
 type LevelPick = Pick<Level, 'id' | 'name' | 'index'>
 
-type ExperiencePick = {
-  id: Experience['id']
-  level: Pick<Level, 'id' | 'index'>
-  skill: Pick<Skill, 'id' | 'name'>
+type SkillPick = {
+  id: Skill['id']
+  name: Skill['name']
+  experiences: [
+    {
+      id: Experience['id']
+      level: Pick<Level, 'id'>
+    },
+  ]
 }
 
 type QueryType = {
   levels: LevelPick[]
-  experiences: ExperiencePick[]
+  skills: SkillPick[]
 }
 
+/**
+ * Getting skills array with the desired length and levels, sort ascending
+ * @param skills - Skill array
+ * @param count - Results number; if omit - return all array
+ * @param levelId - Skills level; if omit - execute for all levels
+ */
+const getHighestSkills = (skills: QueryType['skills'], count: number, levelId?: string) =>
+  skills
+    .map((skill) => ({
+      name: skill.name,
+      rate: levelId
+        ? skill.experiences.filter((el) => el.level.id === levelId).length
+        : skill.experiences.length,
+      link: getSkillLink(skill.id),
+    }))
+    .sort((a, b) => b.rate - a.rate)
+    .slice(0, count)
+
+/**
+ * Render function
+ */
 export default function StatisticsPage() {
   const { data, loading } = useQuery<QueryType>(query, {
     variables: {
@@ -51,39 +84,40 @@ export default function StatisticsPage() {
   })
 
   const levels = data?.levels || []
-  const experiences = data?.experiences
+  const skills = data?.skills || []
 
-  const getExperiencesByLevel = (level: number) =>
-    experiences?.filter(it => it.level.index === level)
-  const getExperiencesNames = (experiences: QueryType['experiences']) =>
-    experiences.map(it => ({
-      key: it.skill.id,
-      value: it.skill.name,
-    }))
+  // Fill the complete skills array
+  const skillsByLevels = [{ index: -1, name: 'Skills', skills: getHighestSkills(skills, 10) }]
 
-  let tabs: any = []
+  levels.forEach((level) =>
+    skillsByLevels.push({
+      index: level.index,
+      name: level.name,
+      skills: getHighestSkills(skills, 5, level.id),
+    }),
+  )
 
-  if (experiences) {
-    tabs.push({
-      title: 'Top skills',
-      key: 'top',
-      body: <BarChart items={getExperiencesNames(experiences)} />,
-    })
+  // Color pallette for bar charts
+  const barPallette = ['#FBBC20', '#88C673', '#70DEC4', '#1890FF', 'red']
 
-    tabs = tabs.concat(
-      levels
-        .sort((a, b) => a.index - b.index)
-        .map(level => {
-          const experiences = getExperiencesByLevel(level.index) || []
-          return {
-            title: level.name,
-            key: level.index.toString(),
-            body: <BarChart items={getExperiencesNames(experiences)} />,
-          }
-        }),
-    )
-  }
+  const { Title } = Typography
 
+  // One instance of bar chart
+  const barChartContainer = (skillId: number) => (
+    <div style={{ marginTop: '50px' }}>
+      <Title
+        level={4}
+      >{`Top ${skillsByLevels[skillId].skills.length} ${skillsByLevels[skillId].name}`}</Title>
+      <BarChart
+        data={skillsByLevels[skillId].skills}
+        dataKey="rate"
+        color={barPallette[skillId]}
+        marginLeft={skillId === 0 ? '-5px' : ''}
+      />
+    </div>
+  )
+
+  // Show skeleton during loading
   if (loading) {
     return (
       <PageContent>
@@ -93,9 +127,23 @@ export default function StatisticsPage() {
   }
 
   return (
-    <>
-      {!experiences && <div>No data provided yet</div>}
-      {experiences && <Tabs tabs={tabs} />}
-    </>
+    <PageContent>
+      <Title level={1}>Skills Statistics</Title>
+      <Row>
+        <Col span={24}>{barChartContainer(0)}</Col>
+      </Row>
+      <Row>
+        <Col span={10}>{barChartContainer(1)}</Col>
+        <Col span={10} offset={2}>
+          {barChartContainer(2)}
+        </Col>
+      </Row>
+      <Row>
+        <Col span={10}>{barChartContainer(3)}</Col>
+        <Col span={10} offset={2}>
+          {barChartContainer(4)}
+        </Col>
+      </Row>
+    </PageContent>
   )
 }
--- src/components/UI/BarChart.tsx
+++ src/components/UI/BarChart.tsx
@@ -1,216 +1,109 @@
+import React, { PureComponent } from 'react'
+import { RouteComponentProps, withRouter } from 'react-router-dom'
+import { useMediaQuery } from 'react-responsive'
+import { BarChart, Bar, XAxis, LabelList, ResponsiveContainer, Text } from 'recharts'
 import { Tooltip } from 'antd'
-import Title from 'antd/lib/typography/Title'
-import React, { useEffect, useRef, useState } from 'react'
-import { RouteComponentProps, withRouter, Link } from 'react-router-dom'
-import styled, { keyframes } from 'styled-components/macro'
-import { getSkillLink } from '../../paths'
 
-const StyledContainer = styled.div`
-  text-overflow: ellipsis;
-  overflow: hidden;
-  white-space: nowrap;
-`
-
-interface SmartTooltipProps {
-  children: any
-  tooltipTitle: string
+interface Props {
+  data: Array<object>
+  dataKey: string
+  color: string
+  marginLeft: string
 }
 
-export const isEllipsisActive = (element: HTMLElement): boolean =>
-  element.offsetWidth < element.scrollWidth
+export default withRouter(
+  ({ data, dataKey, color, marginLeft, history }: Props & RouteComponentProps) => {
+    const isLabelRotate = useMediaQuery({ maxWidth: 960 }) // rotate labels when thin screen
+    const leaveOnlyFirstWordInLabel = useMediaQuery({ maxWidth: 520 }) // cutting all words in label except the first one
 
-const SmartTooltip = ({ children, tooltipTitle }: SmartTooltipProps) => {
-  const [hasEllipsis, setHasEllipsis] = useState(false)
-  const [isTooltipVisible, setTooltipVisibility] = useState(false)
-  const containerRef = useRef(null)
+    const customLabel = (props: any) => {
+      const { x, y, width, height } = props
+      const [name, link] = props.value.split('_')
 
-  useEffect(() => {
-    const containerEl = containerRef.current
-    if (!containerEl) return
+      const MAX_WORD_LENGTH = 10 // length over which words will be cutting
+      const labelAngle = (960 - document.documentElement.clientWidth) * 0.3 // angle for labels rotation on thin screen
 
-    setHasEllipsis(isEllipsisActive(containerEl))
-  }, [setHasEllipsis])
+      const isNameSingleWord = name.split(' ').length === 1 // label is a single word
+      const isFirstWordOverLength = name.split(' ')[0].length > MAX_WORD_LENGTH // first word in label is over length
 
-  return (
-    <StyledContainer ref={containerRef}>
-      <Tooltip title={tooltipTitle} visible={isTooltipVisible}>
-        {React.cloneElement(children, {
-          onMouseOver: () => {
-            if (hasEllipsis) setTooltipVisibility(true)
-          },
-          onMouseLeave: () => setTooltipVisibility(false),
-        })}
-      </Tooltip>
-    </StyledContainer>
-  )
-}
+      const nameTooLong =
+        Math.max.apply(
+          null,
+          name.split(' ').map((word: any) => word.length),
+        ) > MAX_WORD_LENGTH // any of words in then label is over length
 
-const sortDesc = (a: number, b: number) => b - a
-const percentageOf = (total: number) => (value: number) => (value / total) * 100
+      const cuttingName = name
+        .split(' ')
+        .slice(0, leaveOnlyFirstWordInLabel ? 1 : isLabelRotate ? 2 : 100)
+        .map((word: any) =>
+          word.trim().length
+            ? word.length > MAX_WORD_LENGTH
+              ? word.slice(0, MAX_WORD_LENGTH - 3) + '... '
+              : word + ' '
+            : '',
+        )
+        .join('') // general new label with replacement of over length with ellipsis
 
-const countUniqueItems = (arr: { key: string; value: string }[]): Record<string, number> => {
-  if (!Array.isArray(arr)) {
-    return {}
-  }
+      const cuttingFirstWordEllipsis = name.split(' ')[0].slice(0, MAX_WORD_LENGTH - 3) + '... ' // only first word of the label with ellipsis
 
-  return arr.reduce((acc: any, it) => {
-    if (!acc.hasOwnProperty(it.key)) acc[it.key] = 1
-    else acc[it.key] += 1
+      const labeledLink = () => (
+        <a
+          onClick={() => {
+            history.push(link)
+          }}
+        >
+          <Text
+            x={isLabelRotate ? (labelAngle < 60 ? x + width / 2 : x + width) : x}
+            y={height + y + 35}
+            width={20}
+            angle={isLabelRotate ? (labelAngle <= 90 ? labelAngle : 90) : 0}
+            textAnchor="start"
+            verticalAnchor="start"
+            fill="#1890FF"
+            fontFamily="Segoe UI, Arial"
+            fontSize="14px"
+          >
+            {leaveOnlyFirstWordInLabel &&
+            ((isNameSingleWord && isFirstWordOverLength) || !isNameSingleWord)
+              ? cuttingFirstWordEllipsis
+              : cuttingName}
+          </Text>
+        </a>
+      )
 
-    return acc
-  }, {})
-}
-
-const DEFAULT_ROW_COLOR = '#1890ff'
-const MAX_BAR_WIDTH = 500
-
-const StyledBarAnimation = keyframes`
-  0% {
-    max-width: 0;
-  }
-  100% {
-    max-width: ${MAX_BAR_WIDTH}px;
-  }
-`
-
-const StyledPercentageAnimation = keyframes`
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
-`
-
-const StyledChartRow = styled.div`
-  display: flex;
-  align-items: center;
-  margin-bottom: 10px;
-`
-
-const StyledRowValues = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  width: 150px;
-  text-align: right;
-
-  .label {
-    font-weight: 700;
-  }
-  .amount {
-    flex-shrink: 0;
-  }
-  .bold {
-    font-weight: 700;
-  }
-`
-
-const StyledBarWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  flex-grow: 1;
-  max-width: ${MAX_BAR_WIDTH}px;
-  padding-right: 50px;
-`
-
-const StyledBar = styled.div<{ barWidth: number; barColor: string }>`
-  margin: 0 5px;
-  padding-left: 5px;
-  height: 14px;
-  flex-shrink: 0;
-  width: ${({ barWidth }) => barWidth}%;
-  animation-name: ${StyledBarAnimation};
-  animation-timing-function: ease-in-out;
-  animation-duration: 0.6s;
-  animation-delay: 0.3s;
-  animation-fill-mode: backwards;
-  background-color: ${({ barColor }) => barColor};
-`
-
-const StyledPercentage = styled.span`
-  font-weight: 700;
-  animation-name: ${StyledPercentageAnimation};
-  animation-timing-function: ease-in-out;
-  animation-duration: 0.6s;
-  animation-delay: 0.5s;
-  animation-fill-mode: backwards;
-`
-
-interface ChartRowProps extends RouteComponentProps {
-  title: string
-  value: number
-  id: string
-  highestValue: number
-  itemsCount: number
-  rowColor?: string
-}
-
-const ChartRow = withRouter(
-  ({
-    title,
-    id,
-    value,
-    highestValue,
-    itemsCount,
-    history,
-    rowColor = DEFAULT_ROW_COLOR,
-  }: ChartRowProps) => {
-    const calcBarWidth = percentageOf(highestValue)
-    const calcPercentage = percentageOf(itemsCount)
+      return nameTooLong ? (
+        <Tooltip placement="bottomRight" title={name}>
+          {labeledLink()}
+        </Tooltip>
+      ) : (
+        labeledLink()
+      )
+    }
 
     return (
-      <StyledChartRow data-cy="ChartRow">
-        <StyledRowValues>
-          <SmartTooltip tooltipTitle={title}>
-            <Link to={getSkillLink(id)}>{title}</Link>
-          </SmartTooltip>
-          <span className="amount">&nbsp;({value})</span>
-        </StyledRowValues>
-        <StyledBarWrapper>
-          <StyledBar barWidth={calcBarWidth(value)} barColor={rowColor} />
-          <StyledPercentage>{calcPercentage(value).toFixed(1)}%</StyledPercentage>
-        </StyledBarWrapper>
-      </StyledChartRow>
+      <div style={{ width: '100%', height: isLabelRotate ? 330 : 300, marginLeft }}>
+        <ResponsiveContainer>
+          <BarChart
+            data={data}
+            width={900}
+            height={200}
+            barSize={30}
+            maxBarSize={980}
+            margin={{ right: 20 }}
+          >
+            <XAxis
+              dataKey={dataKey}
+              axisLine={false}
+              tickLine={false}
+              height={isLabelRotate ? 110 : 80}
+              tick={{ fontSize: '18px', fontFamily: 'Segoe UI, Arial' }}
+            />
+            <Bar dataKey={dataKey} fill={color} radius={2}>
+              <LabelList dataKey={(skill) => `${skill.name}_${skill.link}`} content={customLabel} />
+            </Bar>
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     )
   },
 )
-
-const entryValueBy = (sortFn: any) => (a: any, b: any) => sortFn(a[1], b[1])
-
-interface Props {
-  items: { key: string; value: string }[]
-  title?: string
-  itemsSliceCount?: number
-}
-
-export default ({ items, title = '', itemsSliceCount = 30 }: Props) => {
-  const barData = countUniqueItems(items)
-  const highestValue = Math.max(...Object.values(barData))
-
-  if (!Object.entries(barData).length)
-    return <div style={{ padding: 16 }}>No statistic collected</div>
-
-  return (
-    <section style={{ padding: '0 16px' }}>
-      <Title level={4} style={{ marginLeft: 50 }}>
-        {title}
-      </Title>
-      {Object.entries(barData)
-        .sort(entryValueBy(sortDesc))
-        .slice(0, itemsSliceCount)
-        .map(([key, value]) => {
-          const item = items.find(i => key === i.key)
-          return (
-            <ChartRow
-              key={item!.key}
-              title={item?.value || 'Unnamed'}
-              id={item!.key}
-              value={value}
-              itemsCount={items.length}
-              highestValue={highestValue}
-            />
-          )
-        })}
-    </section>
-  )
-}
