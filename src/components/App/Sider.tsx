f555be2586a0ea8a9a123b6ab6a334a4102c228a
Alexey Avdeev <alexey.avdeev@sidenis.com>
2020-05-25 09:18:16 +0300

    fixed menu resize

--- src/components/App/Sider.tsx
+++ src/components/App/Sider.tsx
@@ -4,22 +4,27 @@ import styled from 'styled-components'
 import ProfileSider from '../Profile/ProfileSider'
 import Menu from '../UI/Menu'
 import Sider from '../UI/Sider'
+import { useMediaQuery } from 'react-responsive'
+import { COLLAPSE_WIDTH } from '../../config'
 
-const ScrollableWrap = styled.div`
+const ScrollableWrap = styled.div<{ width: number }>`
   position: fixed;
   top: 0px;
   height: 100vh;
   display: flex;
+  width: ${props => props.width + 15}px;
   flex-direction: column;
   padding-bottom: 30px;
   overflow-y: scroll;
   overflow-x: hidden;
+  transition: width 0.1s linear;
 `
 
 function PortalSider({ location }: RouteComponentProps) {
+  const isLarge = useMediaQuery({ minWidth: COLLAPSE_WIDTH })
   return (
     <Sider>
-      <ScrollableWrap>
+      <ScrollableWrap width={isLarge ? 240 : 60}>
         <ProfileSider />
         <Menu path={'/' + location.pathname.split('/')[1]} />
       </ScrollableWrap>
