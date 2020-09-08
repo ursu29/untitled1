5817f412728e9c517e85917a2a09ca382441a4f1
Alexander Vygodchikov <Alexander.Vygodchikov@sidenis.com>
2020-09-08 17:58:23 +0300

    timezone fixed

--- src/components/WorkspacePlanner/index.tsx
+++ src/components/WorkspacePlanner/index.tsx
@@ -242,8 +242,8 @@ export default function WorkspacePlanner() {
           workplace: workplaceId,
           employee: employee.employee.strapiId,
           employeeEmail: employee.employee.email,
-          startDate: dayjs(dateRange.startDate, 'DD.MM.YYYY').toDate(),
-          finishDate: dayjs(dateRange.finishDate, 'DD.MM.YYYY').toDate(),
+          startDate: dayjs(dateRange.startDate + '-0000', 'DD.MM.YYYYZZ').toDate(),
+          finishDate: dayjs(dateRange.finishDate + '-0000', 'DD.MM.YYYYZZ').toDate(),
         },
       },
     })
