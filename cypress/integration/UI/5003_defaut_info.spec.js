53de5181cc320ce8f987f4e2ee2585ae07be100b
Nikolay.Kozub <Nikolay.Kozub@sidenis.com>
2020-10-26 16:49:15 +0300

    updated tests; fixed conflicts; added yarn.lock to ignore;

--- cypress/integration/UI/5003_defaut_info.spec.js
+++ cypress/integration/UI/5003_defaut_info.spec.js
@@ -40,7 +40,7 @@ describe('Checking default information', () => {
       const {data} = res.body;
       allData = {...allData, levels: data}
     })
-  })
+  });
 
   it('Check Employee data', () => {
     const {profile} = allData.client;
@@ -51,7 +51,7 @@ describe('Checking default information', () => {
     cy.haveText('email', profile.email);
     cy.checkIfElementPresent('phone', profile.phoneNumber);
     cy.checkIfElementPresent('bonuses', profile.bonuses)
-  })
+  });
 
   it('Information about employee manager', () => {
     const {manager} = allData.manager.employees[0];
@@ -59,7 +59,7 @@ describe('Checking default information', () => {
     cy.getElement('employee_card').contains(manager.name);
     cy.getElement('employee_card').contains(manager.position);
     cy.getElement('employee_avatar').should('be.visible')
-  })
+  });
 
   it('Check the Projects field', () => {
     const {projects} = allData.projects.employees[0];
@@ -72,7 +72,7 @@ describe('Checking default information', () => {
       return;
     }
     cy.getElement('project').should('not.exist')
-  })
+  });
 
   it('Check the Teams and Outlook buttons', () => {
     cy.getElement('mail_button').should('be.visible');
@@ -88,31 +88,34 @@ describe('Checking default information', () => {
     cy.checkSkills('noExperienced', 'Experienced', 'Experienced', experiences);
     cy.checkSkills('noCurrently', 'Currently', 'Currently studies', experiences);
     cy.checkSkills('noWants', 'Wants', 'Wants to know', experiences);
-  })
+  });
 
   it('Check all tabs', () => {
     Object.values(tabs).forEach(val => cy.getElement(val).contains(val))
-  })
+  });
 
   it('Check the Bookmarks tab', () => {
     cy.getElement(tabs.bookMark).click();
     cy.getElement('bookmark').contains('Add bookmark')
-  })
+  });
 
   it('Check the Matrices tab', () => {
     cy.getElement(tabs.matrices).click();
     cy.getElement('legend-buttons').should('be.visible');
-  })
+  });
+
   it('Check the Personal development tab', () => {
     cy.getElement(tabs.personal).click();
     cy.getElement('evaluation-form').should('be.visible');
-  })
+  });
+
   it('Check the Self Evaluation Form tab', () => {
     cy.getElement(tabs.form).click();
     cy.getElement('helper').should('be.visible');
-  })
+  });
+
   it('Check the CV tab', () => {
     cy.getElement(tabs.cv).click();
     cy.getElement('cv-form').should('be.visible');
-  })
-})
+  });
+});
