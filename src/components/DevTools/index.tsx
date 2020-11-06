a3a8633e5dba2cd5f52d2a01b208f69b35e3aed8
Alexander Vygodchikov <A.Vygodchikov@sidenis.com>
2020-11-06 10:01:42 +0000

    Merged PR 1266: show git info in DevTool
    Related work items: #6055

--- src/components/DevTools/index.tsx
+++ src/components/DevTools/index.tsx
@@ -1,96 +1,175 @@
-import React, { useState } from 'react'
-import { Select } from 'antd'
+import React from 'react'
+import { Select, Space, Tooltip } from 'antd'
 import styled from 'styled-components'
 import { useEmployee } from '../../utils/withEmployee'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
 
 const Wrapper = styled.div`
   position: fixed;
   right: 20px;
-  top: 20px;
-  width: 165px;
+  bottom: 0;
   user-select: none;
-`
-
-const Title = styled.div<{ active: boolean }>`
-  font-size: 11px;
-  font-weight: bold;
-  color: red;
-  cursor: pointer;
-  width: fit-content;
-  user-select: none;
-  filter: ${props => (props.active ? '' : 'opacity(0.4)')};
+  padding-bottom: 20px;
+  filter: opacity(0.4);
   :hover {
-    filter: opacity(0.7);
+    filter: opacity(1);
   }
 `
 
-const ActiveForm = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  border: 1px solid red;
-  border-radius: 2px;
-  padding: 10px;
-  background-color: cornsilk;
+const Title = styled.div`
+  font-size: 10px;
+  font-weight: bold;
+  color: white;
+  width: fit-content;
   user-select: none;
-  background-color: #87ded81c;
+  padding: 1px 3px;
+  background-color: #ffa828;
+  border-radius: 5px;
 `
 
-const NoActiveForm = styled.div`
+const GitInfo = styled.div`
   display: flex;
-  padding: 0 5px;
-  margin-top: -3px;
+  align-items: center;
+  white-space: nowrap;
 `
 
-const TextLine = styled.div`
-  color: rgba(0, 128, 0, 0.82);
-  font-size: 18px;
-  font-weight: 100;
-  letter-spacing: -0.2px;
-  background-color: #87ded81c;
+const Repo = styled.div`
+  color: purple;
+  font-size: 12px;
+  font-weight: 400;
+`
+
+const Branch = styled.div`
+  color: blue;
+  font-size: 12px;
+`
+
+const Commit = styled.div`
+  margin: -5px 0 -5px 10px;
+  color: gray;
+  font-size: 11px;
+  cursor: pointer;
 `
 
 const { Option } = Select
 
 export default function DevTools({ children }: any) {
   const user = useEmployee()
-  const [active, toggleActive] = useState(false)
+
+  // Get ClientDevToolsGitInfo
+  const { data } = useQuery(
+    gql`
+      {
+        clientDevToolsGitInfo {
+          repoName
+          branchName
+          commitId
+          commitMsg
+        }
+      }
+    `,
+  )
+
+  // GATEWAY Git Info
+  const gatewayGitInfo = data?.clientDevToolsGitInfo || {
+    repoName: null,
+    branchName: null,
+    commitId: null,
+    commitMsg: null,
+  }
+  const {
+    repoName: repoNameGW,
+    branchName: branchNameGW,
+    commitId: commitIdGW,
+    commitMsg: commitMsgGW,
+  } = gatewayGitInfo
+
+  // CLIENT Git Info
+  const clientGitInfo = {
+    repoName: process.env.REACT_APP_DT_REPO_NAME,
+    branchName: process.env.REACT_APP_DT_BRANCH_NAME,
+    commitId: process.env.REACT_APP_DT_SOURCE_VERSION,
+    commitMsg: process.env.REACT_APP_DT_SOURCE_VERSION_MSG,
+  }
+  const { repoName, branchName, commitId, commitMsg } = clientGitInfo
 
   return (
     <>
       <Wrapper>
-        <Title active={active} onClick={() => toggleActive(!active)}>
-          DEV TOOLS
-        </Title>
-        {!active ? (
-          <NoActiveForm>
-            <TextLine>{user.devOnlyUserRole.toUpperCase()}</TextLine>
-          </NoActiveForm>
-        ) : (
-          <ActiveForm>
-            <span>Assign new role</span>
-            <Select
-              defaultValue={user.devOnlyUserRole}
-              style={{
-                width: 140,
-                color: 'rgba(0, 128, 0, 0.82)',
-                fontSize: '18px',
-                fontWeight: 100,
-                letterSpacing: '-0.2px',
-              }}
-              bordered={false}
-              onChange={value => {
-                user.setDevOnlyUserRole(value)
-                localStorage.setItem('devOnlyUserRole', value)
-                window.location.reload()
-              }}
-            >
-              <Option value="off">OFF</Option>
-              <Option value="user">USER</Option>
-              <Option value="manager">MANAGER</Option>
-              <Option value="superUser">SUPERUSER</Option>
-            </Select>
-          </ActiveForm>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            marginBottom: '-8px',
+            marginLeft: '-10px',
+          }}
+        >
+          <Title>DevTool</Title>
+          <Select
+            defaultValue={user.devOnlyUserRole}
+            style={{
+              width: 100,
+              color: 'rgba(0, 128, 0, 0.82)',
+              fontSize: '12px',
+              fontWeight: 100,
+              letterSpacing: '-0.2px',
+            }}
+            bordered={false}
+            onChange={value => {
+              user.setDevOnlyUserRole(value)
+              localStorage.setItem('devOnlyUserRole', value)
+              window.location.reload()
+            }}
+          >
+            <Option value="off">OFF</Option>
+            <Option value="user">USER</Option>
+            <Option value="manager">MANAGER</Option>
+            <Option value="superUser">SUPERUSER</Option>
+          </Select>
+        </div>
+
+        {!!Object.values(clientGitInfo).join('') && (
+          <>
+            <GitInfo>
+              <Space>
+                <Repo>{repoName}</Repo>
+                {' - '}
+                <Branch>{branchName}</Branch>
+              </Space>
+            </GitInfo>
+            <GitInfo>
+              <Commit>
+                <Tooltip placement="left" title={commitMsg}>
+                  {commitId?.slice(0, 8)}
+                </Tooltip>
+              </Commit>
+            </GitInfo>
+          </>
+        )}
+
+        {!!Object.values({
+          repoNameGW,
+          branchNameGW,
+          commitIdGW,
+          commitMsgGW,
+        }).join('') && (
+          <>
+            <GitInfo>
+              <Space>
+                <Repo>{repoNameGW}</Repo>
+                {' - '}
+                <Branch>{branchNameGW}</Branch>
+              </Space>
+            </GitInfo>
+            <GitInfo>
+              <Commit>
+                <Tooltip placement="left" title={commitMsgGW}>
+                  {commitIdGW?.slice(0, 8)}
+                </Tooltip>
+              </Commit>
+            </GitInfo>
+          </>
         )}
       </Wrapper>
       {children}
