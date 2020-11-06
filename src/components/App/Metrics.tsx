a29434034e323a4ebeae2c0d0e551337dbeea4fb
Alexey Avdeev <Alexey.Avdeev@Sidenis.com>
2020-11-06 08:19:52 +0000

    Merged PR 1264: added yandex metrika
    added yandex metrika
    
    Related work items: #5928

---
+++ src/components/App/Metrics.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import ym from 'react-yandex-metrika'
+
+export default function () {
+  const location = useLocation()
+
+  useEffect(() => {
+    // TODO: remove /client after migrating api to subdomain.
+    // this is only for result unification (between page hit and location changes)
+    ym('hit', '/client' + location.pathname)
+  }, [location])
+
+  return null
+}
