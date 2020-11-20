f12f3d70d715cbc674e3b50f76530519522cd94b
Nikolay.Kozub <Nikolay.Kozub@sidenis.com>
2020-11-20 18:20:48 +0300

    fixed workspace.spec.js;

--- cypress/integration/BE/employeeNavigation.js
+++ cypress/integration/BE/employeeNavigation.js
@@ -12,39 +12,6 @@ import {
   skills,
 } from '../../support/client/matrices'
 
-describe(`Check employee getBookmarks`, () => {
-  before(() => {
-    cy.setToken('employee')
-    cy.post(getEmployee(email('employee'))).then(res => {
-      const { data } = res.body
-
-      employeeData.employee = { ...data.employeeByEmail }
-    })
-  })
-
-  it('getBookmarks response', () => {
-    const { email, id, name, __typename } = employeeData.employee
-
-    cy.getResponse(['getBookmarks'], 'alias')
-    cy.visit('/client/profile/bookmarks')
-
-    cy.wait(`@alias`).then(val => {
-      const { bookmarks } = JSON.parse(val.response.body).data
-
-      expect(bookmarks).to.be.a('array')
-      cy.compareObjectsKeys(bookmarks[0], bookmark)
-
-      bookmarks.forEach(el => {
-        expect(el.access).to.deep.equal(bookmarkAccess)
-        expect(el.likes).to.be.a('array')
-        expect(el.skills).to.be.a('array')
-        // employee data is present
-        checkKeyValueExist(el.employee, { email, id, name, __typename })
-      })
-    })
-  })
-})
-
 describe(`Check employee matrices`, () => {
   let response
 
@@ -137,6 +104,39 @@ describe(`Check employee matrices`, () => {
       cy.compareObjectsKeys(skill, skillData)
       expect(firstSkill.__typename).equal(skills.__typename)
       expect(skill.__typename).equal(skillData.__typename)
+    })
+  })
+})
+
+describe(`Check employee getBookmarks`, () => {
+  before(() => {
+    cy.setToken('employee')
+    cy.post(getEmployee(email('employee'))).then(res => {
+      const { data } = res.body
+
+      employeeData.employee = { ...data.employeeByEmail }
+    })
+  })
+
+  it('getBookmarks response', () => {
+    const { email, id, name, __typename } = employeeData.employee
+
+    cy.getResponse(['getBookmarks'], 'alias')
+    cy.visit('/client/profile/bookmarks')
+
+    cy.wait(`@alias`).then(val => {
+      const { bookmarks } = JSON.parse(val.response.body).data
+
+      expect(bookmarks).to.be.a('array')
+      cy.compareObjectsKeys(bookmarks[0], bookmark)
+
+      bookmarks.forEach(el => {
+        expect(el.access).to.deep.equal(bookmarkAccess)
+        expect(el.likes).to.be.a('array')
+        expect(el.skills).to.be.a('array')
+        // employee data is present
+        checkKeyValueExist(el.employee, { email, id, name, __typename })
+      })
     })
   })
 })
