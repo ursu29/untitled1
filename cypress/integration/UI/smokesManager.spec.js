c090a6324ee4c734116dcd33a06faab6e9ff8b91
Nikolay.Kozub <Nikolay.Kozub@sidenis.com>
2020-12-02 17:29:25 +0300

    fixed smoke tests

--- cypress/integration/UI/smokesManager.spec.js
+++ cypress/integration/UI/smokesManager.spec.js
@@ -1,12 +1,35 @@
-import Profile from '../../page_objects/profile'
+import { getClient, getManager, getProjects, getEmployeeExperiences } from '../../support/getData'
+import { tabs, workspace } from '../../support/locators'
+import { employeeData } from '../../support/client/employeeData'
 
-describe('Smokes manager', () => {
-  const managerProfile = new Profile()
-  const employeeProfile = new Profile()
+describe('Checking default information', () => {
+  const userId = '1bf931df-2015-4516-ac33-0d2caddc7df2'
+  let allData = {
+    client: null,
+    manager: null,
+    projects: null,
+    levels: null,
+  }
 
-  beforeEach(() => {
+  before(() => {
     cy.setToken('manager')
-    managerProfile.visit()
+    cy.visit('/')
+    cy.post(getClient()).then(res => {
+      const { data } = res.body
+      allData = { ...allData, client: data }
+    })
+    cy.post(getManager(userId)).then(res => {
+      const { data } = res.body
+      allData = { ...allData, manager: data }
+    })
+    cy.post(getProjects(userId)).then(res => {
+      const { data } = res.body
+      allData = { ...allData, projects: data }
+    })
+    cy.post(getEmployeeExperiences(userId)).then(res => {
+      const { data } = res.body
+      allData = { ...allData, levels: data }
+    })
   })
 
   beforeEach(() => {
@@ -16,24 +39,82 @@ describe('Smokes manager', () => {
     cy.saveLocalStorage()
   })
 
-  it('Opens profile by default', () => {
-    managerProfile.url().should('include', '/client/profile')
-    managerProfile.avatar().should('be.visible')
-    managerProfile.name().should('contain', 'Test Manager')
-    managerProfile.jobTitle().should('contain', 'Test')
-    managerProfile.city().should('contain', 'Saint-Petersburg')
+  it('Check Employee data', () => {
+    const { profile } = allData.client
+    const { manager } = allData.manager.employees[0]
+
+    cy.haveText('employee_name', profile.name)
+    cy.haveText('position', profile.position)
+    cy.haveText('location', profile.location)
+    cy.haveText('email', profile.email)
+    cy.checkIfElementPresent('phone', profile.phoneNumber)
+    cy.checkIfElementPresent('bonuses', profile.bonuses)
+    cy.get('.ant-card-meta-description').contains('Agile').should('have.text', manager.position)
+    cy.getElement('avatar').should('be.visible')
   })
 
-  it('Manager can see own private tabs', () => {
-    managerProfile.tabMatrices().should('be.visible')
-    managerProfile.tabPersonalDevelopment().should('be.visible')
-    managerProfile.tabSelfEvaluationForm().should('be.visible')
+  it('Check the Projects field', () => {
+    const { projects } = allData.projects.employees[0]
+
+    if (projects.length) {
+      const projectNames = projects.map(obj => obj.name)
+      cy.checkLength('project_tab', projects.length)
+      cy.checkTextInArrayEl('project_tab', projectNames)
+
+      return
+    }
+    cy.getElement('project').should('not.exist')
+  })
+
+  it('Check the Teams and Outlook buttons', () => {
+    cy.getElement('mail_button').should('be.visible')
+    cy.getElement('teams_button').should('be.visible')
+  })
+
+  it('Check the Skills tab', () => {
+    const { experiences } = allData.levels.employees[0]
+
+    cy.get(workspace.tab).contains(tabs.skill)
+    cy.getElement('Title Confident in').contains('Confident in')
+    cy.checkSkills('noConfident', 'Confident', 'Confident in', experiences)
+    cy.checkSkills('noExperienced', 'Experienced', 'Experienced', experiences)
+    cy.checkSkills('noCurrently', 'Currently', 'Currently studies', experiences)
+    cy.checkSkills('noWants', 'Wants', 'Wants to know', experiences)
+  })
+
+  it('Check all tabs', () => {
+    Object.values(tabs).forEach(val => cy.get(workspace.tab).contains(val))
+  })
+
+  it('Check the Bookmarks tab', () => {
+    cy.get(workspace.tab).contains(tabs.bookMark).click()
+    cy.getElement('bookmark').contains('Add bookmark')
+  })
+
+  it('Check the Matrices tab', () => {
+    cy.get(workspace.tab).contains(tabs.matrices).click()
+    cy.getElement('legend-buttons').should('be.visible')
+  })
+
+  it('Check the Personal development tab', () => {
+    cy.get(workspace.tab).contains(tabs.personal).click()
+    cy.getElement('evaluation-form').should('be.visible')
+  })
+
+  it('Check the Self Evaluation Form tab', () => {
+    cy.get(workspace.tab).contains(tabs.form).click()
+    cy.getElement('helper').should('be.visible')
+  })
+
+  it('Check the CV tab', () => {
+    cy.get(workspace.tab).contains(tabs.cv).click()
+    cy.getElement('cv-form').should('be.visible')
   })
 
   it('Manager can see employee private tabs', () => {
-    cy.openProfile('test.employee')
-    employeeProfile.tabMatrices().should('be.visible')
-    employeeProfile.tabPersonalDevelopment().should('be.visible')
-    employeeProfile.tabSelfEvaluationForm().should('be.visible')
+    cy.visit(`/client/employees/${employeeData.employee.email}`)
+    ;[tabs.matrices, tabs.personal, tabs.cv, tabs.form].forEach(el =>
+      cy.get(workspace.tab).contains(el).should('be.visible'),
+    )
   })
 })
