ecd22d46b3692ab775c1c619077d4dfc5313f76d
Nikolay.Kozub <Nikolay.Kozub@sidenis.com>
2021-01-26 16:22:59 +0300

    added more simply support spec.ts files

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,24 +1,8 @@
-/// <reference types = "node" />
-
-const webpack = require("@cypress/webpack-preprocessor");
 
 module.exports = (on, config, bool = false) => {
-  const options = {
-    webpackOptions: require("../../webpack.config"),
-    watchOptions: {}
-  };
-  on("file:preprocessor", getWepPackWithFileChange(options));
-
   if (bool) {
     require('@cypress/code-coverage/task')(on, config)
+
     return config
   }
 };
-
-function getWepPackWithFileChange(options) {
-  const webPackPreProcessor = webpack(options);
-  return function(file) {
-    file.outputPath = file.outputPath.replace(".ts", ".js");
-    return webPackPreProcessor(file);
-  };
-}
