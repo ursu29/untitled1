31735248e3a7944ff2d8e424fe54ff83ce9e4794
NikolayKozub <Nikolay.Kozub@syncretis.com>
2021-03-01 16:03:42 +0300

    fixed tests + added png

--- e2e/cypress/integration/UI/oneTwoOne.spec.js
+++ e2e/cypress/integration/UI/oneTwoOne.spec.js
@@ -3,29 +3,19 @@ import { response, oneTwoOneLocators } from '../../support/client/oneTwoOne'
 import { skillEl } from '../../support/locators'
 import {getEmployee} from "../../support/getData";
 
-describe('Check oneTwoOne request', () => {
+describe('Check oneTwoOne request by employee', () => {
   let employeeData
 
-  before(() => {
-    cy.setToken('employee')
-    cy.setImgToken('employee')
-    cy.post(getEmployee(email('employee'))).then(res => employeeData = res.body.data.employeeByEmail)
-  })
-
-  beforeEach(() => {
-    cy.restoreLocalStorage()
-  })
-  afterEach(() => {
-    cy.saveLocalStorage()
-  })
-
-  context('Check oneTwoOne request', () => {
     before(() => {
+      cy.setToken('employee')
+      cy.setImgToken('employee')
+      cy.post(getEmployee(email('employee'))).then(res => employeeData = res.body.data.employeeByEmail)
+    })
+    it('Check oneTwoOne popup', () => {
       cy.mockResponse(['one2oneRequest'], response(employeeData.id, false)).as('alias')
       cy.visit('/')
       cy.wait('@alias').then(() => cy.getElement(oneTwoOne.button).click())
-    })
-    it('Check oneTwoOne popup', () => {
+
       cy.get(popUp.title).should('contain.text', oneTwoOne.text)
       cy.get(popUp.button).contains('No').click()
       cy.get(oneTwoOne.disableBtn).should('not.exist')
@@ -45,28 +35,34 @@ describe('Check oneTwoOne request', () => {
       cy.get(popUp.button).contains('Yes').click()
       cy.get(skillEl.successMes).should('have.text', 'Updated')
     })
-  })
+})
 
-  context('Check Manager oneTwoOne', () => {
+describe('Check oneTwoOne request by manager', () => {
+  let employeeData
     before(() => {
+      cy.window().then((window) => {
+        window.sessionStorage.clear();
+        window.localStorage.clear();
+      })
       cy.setToken('manager')
       cy.setImgToken('manager')
+      cy.post(getEmployee(email('employee'))).then(res => employeeData = res.body.data.employeeByEmail)
 
       cy.visit('client/profile/employees')
     })
 
-    beforeEach(() => {
-      cy.restoreLocalStorage()
-    })
-    afterEach(() => {
-      cy.saveLocalStorage()
-    })
+  beforeEach(() => {
+    cy.restoreLocalStorage()
+  })
+  afterEach(() => {
+    cy.saveLocalStorage()
+  })
 
     it('Confirm meeting', () => {
       cy.get(oneTwoOneLocators.bargeSm).should('exist')
       cy.get(oneTwoOneLocators.bargeCount)
-        .eq(0)
-        .then(el => expect(el.text()).contains('1-2-1'))
+          .eq(0)
+          .then(el => expect(el.text()).contains('1-2-1'))
     })
 
     it('Confirm request', () => {
@@ -84,5 +80,4 @@ describe('Check oneTwoOne request', () => {
       cy.get(popUp.button).contains('Yes').click()
       cy.get(skillEl.successMes).should('have.text', 'Updated')
     })
-  })
 })
