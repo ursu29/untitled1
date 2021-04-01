22c8722e3c7f678bfd0571ae4dac658cb3046302
NikolayKozub <Nikolay.Kozub@syncretis.com>
2021-04-01 13:35:32 +0300

    fixed some old tests

--- e2e/cypress/integration/UI/guilds/editTitleLeads.spec.js
+++ e2e/cypress/integration/UI/guilds/editTitleLeads.spec.js
@@ -8,7 +8,7 @@ import {todaysDate} from "../../../support/officePlanner/officeDays";
 describe('Update guilds title and leads', () => {
     let guildInfo
     const {itemsSelect, edit, close} = postEl
-    const {update, title, lead, submit, employeeCard} = guildElements
+    const {update, title, submit, employeeCard} = guildElements
 
     before(() => {
         cy.setToken('manager')
@@ -33,7 +33,7 @@ describe('Update guilds title and leads', () => {
         cy.getId(title).type(todaysDate)
 
         //select value
-        cy.getElement(lead).click()
+        cy.get('span').contains('employee').click()
         cy.get(itemsSelect).eq(0).click()
         cy.get(edit).click()
 
