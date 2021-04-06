a2ddb7879912214152e0758ea0855f41116b3ef4
Evgeny.Mokeev <evgeny.mokeev@syncretis.com>
2021-04-06 13:24:55 +0700

    #16744: update button text

--- src/components/EmployeeEvaluation/EmployeeEvaluation.tsx
+++ src/components/EmployeeEvaluation/EmployeeEvaluation.tsx
@@ -112,7 +112,7 @@ function EmployeeEvaluation({ employee, editable }: Props) {
               createdAt: e.createdAt,
             }))}
           isButtonVisible={true}
-          buttonText="Create New Version"
+          buttonText="Add New"
           tooltip="Your current form will be archived and you will receive a new blank one"
         />
       )}
