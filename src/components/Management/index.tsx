f3f141dc91c2adeb7c85c8d89500b466b9d3be4c
Alexander Vygodchikov <a.vygodhikov@syncretis.com>
2021-05-14 16:43:09 +0300

    Add agile managers tab to management page

--- src/components/Management/index.tsx
+++ src/components/Management/index.tsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import EmployeesTable from './EmployeesTable'
+import React, { useState } from 'react'
+import Employees from './Employees'
+import Agile from './Agile'
 import PageContent from '../UI/PageContent'
 import PageHeader from '../UI/PageHeader'
+import { Tabs } from 'antd'
+import URLAction from '../../utils/URLAction'
 
 export default function Management() {
+  const urlAction = new URLAction()
+  const [view, setView] = useState(urlAction.paramsGet('tab') || 'employees')
+
   return (
     <>
-      <PageHeader title="Management" />
-      <PageContent style={{ paddingLeft: 0, paddingRight: 0 }}>
-        <EmployeesTable />
+      <PageHeader title="Management" withoutDivider />
+      <PageContent style={{ paddingLeft: 0, paddingRight: 0, marginTop: '-32px' }}>
+        <Tabs
+          defaultActiveKey={view}
+          onTabClick={key => {
+            setView(key)
+            urlAction.paramsSet('tab', key)
+          }}
+          tabBarStyle={{ padding: '0 0 0 24px' }}
+        >
+          <Tabs.TabPane tab="Employees" key="employees" />
+          <Tabs.TabPane tab="Agile Managers" key="agile" />
+          <Tabs.TabPane tab="Scrum Masters" key="scrum" />
+        </Tabs>
+
+        {view === 'employees' && <Employees />}
+        {view === 'agile' && <Agile />}
       </PageContent>
     </>
   )
