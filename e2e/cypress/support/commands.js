0e9adc57ef12465cff3af166d90095e43e14d851
NikolayKozub <Nikolay.Kozub@syncretis.com>
2021-06-18 11:48:07 +0300

    added threshold for pixel

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -5,7 +5,7 @@ import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command'
 addMatchImageSnapshotCommand({
   failureThreshold: 0.00, // threshold for entire image
   failureThresholdType: 'percent', // percent of image or number of pixels
-  customDiffConfig: { threshold: 0.0 },  // threshold for each pixel
+  customDiffConfig: { threshold: 0.1 },  // threshold for each pixel
   capture: 'viewport',  // capture viewport in screenshot
   customDiffDir: './TestReport/assets' // screenshotsFolder
 })
