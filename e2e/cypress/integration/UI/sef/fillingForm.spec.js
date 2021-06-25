62c862e1e7b8801df7c49c672ddb3b08e8cf407b
NikolayKozub <Nikolay.Kozub@syncretis.com>
2021-06-25 18:23:59 +0300

    checked self Evaluation form

---
+++ e2e/cypress/integration/UI/sef/fillingForm.spec.js
@@ -0,0 +1,42 @@
+import { matrix, notificationsText, selfEvalForm } from '../../../support/locators'
+import { getTabUrl } from '../../../support/utils'
+import { todaysDate } from '../../../support/officePlanner/officeDays'
+
+describe('update self Evaluation form', () => {
+  const {needsBtn, exceedsBtn, meetsBtn, fillComment} = selfEvalForm
+  const TEXT = `feedback: ${todaysDate}`
+
+  before(() => {
+    cy.setToken('manager')
+    cy.setImgToken('manager')
+  })
+
+  it('successfully filled out the form state:Needs ImprovementMeets, requirements, Exceeds requirements', () => {
+    cy.visit(getTabUrl('evaluation'))
+
+    ;[needsBtn, meetsBtn, exceedsBtn].forEach(el => {
+      cy.getElement(el).first().click({force: true})
+
+      cy.get(matrix.success).should('be.visible')
+      cy.get(matrix.success).should('contain.text', notificationsText.updateSeF)
+      cy.get(matrix.success).should('not.exist')
+    })
+  })
+
+  it('add new comment', () => {
+    cy.getElement(fillComment).first().click({force: true})
+    cy.get('.ant-input').last().clear().type(TEXT)
+    cy.get('span').contains('Cancel').click()
+    cy.get(matrix.success).should('not.exist')
+
+    cy.getElement(fillComment).first().click({force: true})
+    cy.get('span').contains('Post').click()
+
+    cy.get(matrix.success).should('be.visible')
+    cy.get(matrix.success).should('contain.text', notificationsText.updateSeF)
+    cy.get(matrix.success).should('not.exist')
+
+    cy.getElement(fillComment).first().click({force: true})
+    cy.get('.ant-input').last().then(el => expect(el.text()).equal(TEXT))
+  })
+})
