2bb374495fc45a92c22729806f4577e5db89dfb5
Evgeny.Mokeev <evgeny.mokeev@syncretis.com>
2021-07-05 19:33:01 +0700

    #17544: show actions on hover

--- src/components/Employees/EmployeeCVExperience.tsx
+++ src/components/Employees/EmployeeCVExperience.tsx
@@ -9,13 +9,14 @@ import {
   Popconfirm,
   Select,
   Typography,
-  Card,
   Tag,
   Space,
 } from 'antd'
+import { EditOutlined, CheckOutlined } from '@ant-design/icons'
 import moment from 'moment'
 import React, { useState } from 'react'
 import { debounce } from 'throttle-debounce'
+import styled from 'styled-components'
 import message from '../../message'
 import { useUpdateCvMutation } from '../../queries/cv'
 import queryProjects, { QueryType as ProjectsQueryType } from '../../queries/getProjects'
@@ -155,9 +156,11 @@ const JobListView = ({
     <div>
       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
         <Title level={5}>Work experience</Title>
-        <Button disabled={loading} onClick={() => handleCreate('')}>
-          Add Company
-        </Button>
+        {editable && (
+          <Button disabled={loading} onClick={() => handleCreate('')}>
+            Add Company
+          </Button>
+        )}
       </div>
       <div>
         {Object.entries(groupedByCompany).map(([company, data]) => (
@@ -176,6 +179,59 @@ const JobListView = ({
   )
 }
 
+const CardActions = styled.div``
+
+const CardTitle = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+`
+const CardHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  ${CardActions} {
+    opacity: 0;
+    transition: opacity 0.3s;
+  }
+  &:hover {
+    ${CardActions} {
+      opacity: 1;
+    }
+  }
+`
+
+const StyledCompanyHead = styled(CardHeader)`
+  margin: 16px 0;
+`
+
+const StyledProjectCard = styled.div`
+  border: 1px solid #dedede;
+  margin-bottom: 16px;
+`
+const StyledProjectHead = styled(CardHeader)`
+  background: #fafafa;
+  border-bottom: 1px solid #dedede;
+  padding: 8px;
+`
+const StyledProjectTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`
+const StyledProjectName = styled.span`
+  font-size: 18px;
+`
+const StyledProjectBody = styled.div`
+  border-bottom: 1px solid #dedede;
+  padding: 8px 16px 16px;
+`
+const StyledProjectForm = styled(Form)`
+  width: 100%;
+  .ant-form-item {
+    flex: 1;
+  }
+`
+
 const JobView = ({
   data,
   company,
@@ -191,39 +247,56 @@ const JobView = ({
   onDelete: (vitae: Vitae[]) => void
   onCreate: (company: string) => void
 }) => {
+  const [isEditMode, setIsEditMode] = useState(false)
   return (
     <div>
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        {editable ? (
-          <>
-            <AutoComplete
-              style={{ width: '100%' }}
-              options={syncretisNames.map(value => ({ value }))}
-              defaultValue={company}
-              onBlur={event => {
-                // @ts-expect-error
-                const nextCompanyName = event.target.value || ''
-                onUpdate(data.map(p => ({ ...p, company: nextCompanyName })))
-              }}
-              filterOption={(inputValue, option) =>
-                option!.value.toUpperCase().startsWith(inputValue.toUpperCase())
-              }
-              // disabled={!editable}
-            />
-
-            <Space>
-              <Popconfirm title="Sure to delete?" onConfirm={() => onDelete(data)}>
-                <Button type="link" danger>
-                  Delete
-                </Button>
-              </Popconfirm>
-              <Button onClick={() => onCreate(company)}>Add Project</Button>
-            </Space>
-          </>
-        ) : (
-          <Title level={5}>{company || 'Unknown company'}</Title>
+      <StyledCompanyHead>
+        <CardTitle>
+          {editable && isEditMode ? (
+            <>
+              <AutoComplete
+                style={{ width: '100%' }}
+                options={syncretisNames.map(value => ({ value }))}
+                defaultValue={company}
+                onBlur={event => {
+                  // @ts-expect-error
+                  const nextCompanyName = event.target.value || ''
+                  onUpdate(data.map(p => ({ ...p, company: nextCompanyName })))
+                }}
+                filterOption={(inputValue, option) =>
+                  option!.value.toUpperCase().startsWith(inputValue.toUpperCase())
+                }
+              />
+              <Button type="link" icon={<CheckOutlined />} onClick={() => setIsEditMode(false)} />
+            </>
+          ) : (
+            <>
+              <Title level={5} style={{ margin: 0 }}>
+                {company || 'Unknown company'}
+              </Title>
+              {editable && !isEditMode && (
+                <CardActions>
+                  <Button type="link" icon={<EditOutlined />} onClick={() => setIsEditMode(true)} />
+                </CardActions>
+              )}
+            </>
+          )}
+        </CardTitle>
+        {editable && (
+          <Space>
+            {!isEditMode && (
+              <CardActions>
+                <Popconfirm title="Sure to delete?" onConfirm={() => onDelete(data)}>
+                  <Button type="link" danger>
+                    Delete
+                  </Button>
+                </Popconfirm>
+              </CardActions>
+            )}
+            <Button onClick={() => onCreate(company)}>Add Project</Button>
+          </Space>
         )}
-      </div>
+      </StyledCompanyHead>
 
       <div>
         {data.map(p => (
@@ -248,38 +321,29 @@ type ProjectBasicFormValues = {
 
 const ProjectBasicForm = ({
   data,
-  editable,
   onSubmit,
 }: {
   data: Vitae
-  editable: boolean
   onSubmit: (values: ProjectBasicFormValues) => void
 }) => {
   const [form] = Form.useForm<ProjectBasicFormValues>()
   const { data: data2 } = useQuery<ProjectsQueryType>(queryProjects)
   const allProjectsList = data2?.projects?.slice().sort((a, b) => (a.name > b.name ? 1 : -1)) || []
 
-  if (!editable) {
-    if (!data.project && !data.position && !data.level)
-      return <span style={{ fontSize: 18 }}>Unknown project</span>
-    return (
-      <Space>
-        {data.project && <Tag style={{ fontSize: 14 }}>{data.project}</Tag>}
-        <span style={{ fontSize: 18 }}>{data.position}</span>
-        {data.level && <Tag>{data.level}</Tag>}
-      </Space>
-    )
-  }
-
   return (
-    <Form form={form} layout="inline" name={`cv-project-basic-${data.id}`} onFinish={onSubmit}>
+    <StyledProjectForm
+      form={form}
+      layout="inline"
+      name={`cv-project-basic-${data.id}`}
+      // @ts-expect-error
+      onFinish={onSubmit}
+    >
       <Form.Item name="project" label="Project" initialValue={data.project}>
         <Select
           showSearch
           style={{ width: '100%' }}
           placeholder="Select a project"
           optionFilterProp="children"
-          disabled={!editable}
           filterOption={(input, option) =>
             option?.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
           }
@@ -298,7 +362,7 @@ const ProjectBasicForm = ({
       <Form.Item name="level" label="Level" initialValue={data.level}>
         <Input onBlur={form.submit} />
       </Form.Item>
-    </Form>
+    </StyledProjectForm>
   )
 }
 
@@ -308,7 +372,7 @@ type ProjectDetailedFormValues = {
   responsibilities?: string | null
 }
 
-const ProjectDetaledForm = ({
+const ProjectDetailedForm = ({
   data,
   editable,
   onSubmit,
@@ -370,6 +434,7 @@ const ProjectDetaledForm = ({
         name="responsibilities"
         label="Responsibilities"
         initialValue={data?.responsibilities}
+        style={{ marginBottom: 0 }}
       >
         <Input.TextArea
           placeholder="Enter something"
@@ -379,6 +444,19 @@ const ProjectDetaledForm = ({
         />
       </Form.Item>
     </Form>
+  )
+}
+
+const ProjectHeader = ({ data }: { data: Vitae }) => {
+  if (!data.project && !data.position && !data.level) {
+    return <StyledProjectName>Unknown project</StyledProjectName>
+  }
+  return (
+    <>
+      {data.project && <Tag style={{ fontSize: 14 }}>{data.project}</Tag>}
+      <StyledProjectName>{data.position}</StyledProjectName>
+      {data.level && <Tag>{data.level}</Tag>}
+    </>
   )
 }
 
@@ -393,31 +471,45 @@ const ProjectView = ({
   onUpdate: (vitae: Vitae) => void
   onDelete: () => void
 }) => {
+  const [isEditMode, setIsEditMode] = useState(false)
   return (
-    <Card
-      title={
-        <ProjectBasicForm
+    <StyledProjectCard>
+      <StyledProjectHead>
+        <CardTitle>
+          {editable && isEditMode ? (
+            <StyledProjectTitle>
+              <ProjectBasicForm data={data} onSubmit={values => onUpdate({ ...data, ...values })} />
+              <Button type="link" icon={<CheckOutlined />} onClick={() => setIsEditMode(false)} />
+            </StyledProjectTitle>
+          ) : (
+            <Space>
+              <ProjectHeader data={data} />
+              {editable && !isEditMode && (
+                <CardActions>
+                  <Button type="link" icon={<EditOutlined />} onClick={() => setIsEditMode(true)} />
+                </CardActions>
+              )}
+            </Space>
+          )}
+        </CardTitle>
+        {editable && !isEditMode && (
+          <CardActions>
+            <Popconfirm title="Sure to delete?" onConfirm={onDelete}>
+              <Button type="link" danger>
+                Delete
+              </Button>
+            </Popconfirm>
+          </CardActions>
+        )}
+      </StyledProjectHead>
+      <StyledProjectBody>
+        <ProjectDetailedForm
           data={data}
           editable={editable}
           onSubmit={values => onUpdate({ ...data, ...values })}
         />
-      }
-      extra={
-        editable && (
-          <Popconfirm title="Sure to delete?" onConfirm={onDelete}>
-            <Button type="link" danger>
-              Delete
-            </Button>
-          </Popconfirm>
-        )
-      }
-    >
-      <ProjectDetaledForm
-        data={data}
-        editable={editable}
-        onSubmit={values => onUpdate({ ...data, ...values })}
-      />
-    </Card>
+      </StyledProjectBody>
+    </StyledProjectCard>
   )
 }
 
