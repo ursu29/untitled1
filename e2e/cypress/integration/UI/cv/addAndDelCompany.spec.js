56b6409a2649cee99b343a0fb7b6e97184f49e6c
Ursula Kokshinova <ursula.kokshinova@syncretis.com>
2021-08-11 12:39:20 +0700

    Testing CV2

--- e2e/cypress/integration/UI/cv/addAndDelCompany.spec.js
+++ e2e/cypress/integration/UI/cv/addAndDelCompany.spec.js
@@ -14,10 +14,12 @@ describe('successfully create and delete new Company', () => {
         cy.get('.ant-col.ant-col-sm-16 [type="button"]').click()
         cy.get('.ant-select-selection-search input[id="cv-company-name-_company"]').type('New Company')
         cy.get('.anticon.anticon-check').click()
-        cy.get('.sc-iAyFgw > .sc-kEYyzF').should('be.visible')
+        cy.get('.sc-kEYyzF.egAstc').should('exist')
+
         cy.get('[style=""] > .sc-hMqMXs > .ant-btn').click()
         cy.get('.ant-popover-buttons > .ant-btn-primary').click()
-        cy.get('.sc-iAyFgw > .sc-kEYyzF').should('not.exist')
+        cy.get('.sc-kEYyzF.egAstc').should('not.exist')
     })
+
 })
 
