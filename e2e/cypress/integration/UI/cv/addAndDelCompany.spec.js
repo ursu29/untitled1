614478eab9bf8f41169615f7e9ef748f709ace42
Ursula Kokshinova <ursula.kokshinova@syncretis.com>
2021-08-13 12:13:47 +0700

    Testing adding and deleting new Company

--- e2e/cypress/integration/UI/cv/addAndDelCompany.spec.js
+++ e2e/cypress/integration/UI/cv/addAndDelCompany.spec.js
@@ -1,25 +1,24 @@
-import {getCV} from '../../../support/getData'
 import { getSubTabUrl } from '../../../support/utils'
 
 describe('successfully create and delete new Company', () => {
-    before(() => {
 
+    const companyName = 'New Company'
+
+   before(() => {
         cy.setToken('employee')
         cy.visit(getSubTabUrl('career', '/profile', 'cv'))
-
     })
 
     it('Company created and deleted successful', () => {
 
-        cy.get('.ant-col.ant-col-sm-16 [type="button"]').click()
-        cy.get('.ant-select-selection-search input[id="cv-company-name-_company"]').type('New Company')
-        cy.get('.anticon.anticon-check').click()
-        cy.get('.sc-kEYyzF.egAstc').should('exist')
+        cy.getElement('addCompany').click()
+        cy.getId('cv-company-name-_company').type(companyName)
+        cy.getId('cv-company-name-').getType('submit').click()
+        cy.getElement('workExperienceBlock').contains(companyName).should('exist')
 
-        cy.get('[style=""] > .sc-hMqMXs > .ant-btn').click()
-        cy.get('.ant-popover-buttons > .ant-btn-primary').click()
-        cy.get('.sc-kEYyzF.egAstc').should('not.exist')
+        cy.getElement('deleteCompany').click()
+        cy.get('button').contains('OK').click()
+        cy.getElement('workExperienceBlock').contains(companyName).should('not.exist')
     })
-
 })
 
