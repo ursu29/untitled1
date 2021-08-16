aad0884ae1c3a220770f85392bc2cd31cf7b4fd0
KseniyaGerdt <Kseniya.Gerdt@sincretis.ru>
2021-08-16 14:40:33 +0700

    fixed test!

--- e2e/cypress/integration/UI/addTechnologyToProject.js
+++ e2e/cypress/integration/UI/addTechnologyToProject.js
@@ -1,12 +1,11 @@
-import {
-} from '../../support/getData'
+import {} from '../../support/getData'
 import { skillEl } from '../../support/locators'
 
 describe('check updateTechnologiesProject', () => {
 
-  let nameTechnology = 'People Skills';
-  const {skillsEvent} = skillEl
-  const {headerTableSkills} = skillEl
+  let nameTechnology = 'People Skills'
+  const { skillsEvent } = skillEl
+  const { headerTableSkills } = skillEl
 
   let close = '[data-icon="close"]'
   let idSkillsButton = 'rc-tabs-0-tab-skills'
@@ -19,14 +18,14 @@ describe('check updateTechnologiesProject', () => {
 
   it('add new technology', () => {
 
-     cy.getIcon('edit')
-       .click()
-     cy.get(skillsEvent)
-       .click()
-     cy.getTitle(nameTechnology)
-       .click()
-     cy.getIcon('check')
-       .click()
+    cy.getIcon('edit')
+      .click()
+    cy.get(skillsEvent)
+      .click()
+    cy.getTitle(nameTechnology)
+      .click()
+    cy.getIcon('check')
+      .click()
 
     cy.getId(idSkillsButton).click()
     cy.get(headerTableSkills).should('contain.text', nameTechnology)
